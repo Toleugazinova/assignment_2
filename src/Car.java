7730aec6bed6f5c8f65cff55a5af0a8187a4c1eb
Toleugazinova <250272@astanait.edu.kz>
2026-01-03 22:24:00 +0500

    Update Car.java

--- src/Car.java
+++ src/Car.java
@@ -1,4 +1,29 @@
-package PACKAGE_NAME;
+public class Car extends Vehicle {
+    protected int numberOfDoors;
 
-public class Car {
+    public Car(String model, int year, double basePrice, int numberOfDoors) {
+        super(model, year, basePrice);
+        this.numberOfDoors = numberOfDoors;
+    }
+
+    @Override
+    public double calculateInsuranceFee() {
+        return super.getBasePrice() * 0.2;
+    }
+
+    @Override
+    public void performService() {
+        if (getServiceIntervalKm() > 5000) {
+            System.out.println("A server is required for the car "+ getId());
+        }
+    }
+
+    @Override
+    public int getServiceIntervalKm() {
+        return 10000;
+    }
+
+    public String toString() {
+        return "Car " + super.toString() + ", number of doors = " + numberOfDoors;
+    }
 }
