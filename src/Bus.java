7847146dcced5b97c9ca00db77ab06a2ce8e3e50
Toleugazinova <250272@astanait.edu.kz>
2026-01-03 22:23:24 +0500

    Update Bus.java

--- src/Bus.java
+++ src/Bus.java
@@ -1,4 +1,29 @@
-package PACKAGE_NAME;
+public class Bus extends Vehicle {
+    protected int passengerCapacity;
 
-public class Bus {
+    public Bus(String model, int year, double basePrice, int passengerCapacity) {
+        super(model, year, basePrice);
+        this.passengerCapacity = passengerCapacity;
+    }
+
+    @Override
+    public double calculateInsuranceFee(){
+        return getBasePrice()*0.4;
+    }
+
+    @Override
+    public void performService() {
+        if (getServiceIntervalKm()>5000){
+            System.out.println("A server is required for the bus "+ getId());
+        }
+    }
+
+    @Override
+    public int getServiceIntervalKm() {
+        return 1500;
+    }
+
+    public String toString() {
+        return "Bus " + super.toString() + ", capacity = " + passengerCapacity;
+    }
 }
